3b16ee926e67d5dccdb202db0c4208bdbff76f98
adens <asepindrakurniawan@gmail.com>
2023-04-27 13:19:28 +0700

    first commit

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            body {
+                font-family: "Arial", sans-serif;
+                font-size: larger;
+            }
+
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="center">
+            Hello there
+        </div>
+    </body>
+</html>
